8dab7c8b5b749b7be446af2952dca37cfc235f84
Anatolii <anatoliy.petrenko.777@gmail.com>
2023-01-10 20:24:27 +0200

    add https to request

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -6,13 +6,6 @@ import styles from './Main.module.css';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-const URL = {
-  main: 'http://www.omdbapi.com/',
-  apikey: API_KEY,
-};
-
-function createURL() {}
-
 class Main extends React.Component {
   state = {
     movies: [],
@@ -20,20 +13,24 @@ class Main extends React.Component {
   };
 
   componentDidMount() {
-    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=star%20wars&r=json`)
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=star%20wars&r=json`)
       .then(response => response.json())
-      .then(data => this.setState({ movies: data.Search, loading: false }));
+      .then(data => this.setState({ movies: data.Search, loading: false }))
+      .catch(error => {
+        console.log(error);
+        this.setState({ loading: false });
+      });
   }
 
   searchMovies = (str, type = 'all') => {
     this.setState({ loading: true });
     console.log(
-      `http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
         type !== 'all' ? `&type=${type}` : ''
       }&r=json`
     );
     fetch(
-      `http://www.omdbapi.com/?apikey=c49abfca&s=${str}${
+      `https://www.omdbapi.com/?apikey=c49abfca&s=${str}${
         type !== 'all' ? `&type=${type}` : ''
       }&r=json`
     )
